d1520ce68d346e34a4a009f6c1d1fda42d5692e7
Bruno Paixão <brunomrpx@gmail.com>
2015-10-19 23:16:16 -0200

    Added editor config and adjustment on indentation

--- gulpfile.js
+++ gulpfile.js
@@ -10,56 +10,57 @@ var rename = require('gulp-rename');
 gulp.task('default', ['build', 'sass']);
 
 gulp.task('build', function() {
-    gulp
-        .src([
-            'src/application/application.module.js',
-            'src/application/**/*.factory.js',
-            'src/application/**/*.service.js',
-            'src/application/**/*.controller.js',
-            'src/application/**/*.provider.js',
-            'src/application/**/*.directive.js',
-        ])
-        .pipe(concat('application.js'))
-        .pipe(minify({
-            mangle: false
-        }))
-        .pipe(gulp.dest('cordova/www/js/'));
+  gulp
+  .src([
+    'src/application/application.module.js',
+    'src/application/**/*.factory.js',
+    'src/application/**/*.service.js',
+    'src/application/**/*.controller.js',
+    'src/application/**/*.provider.js',
+    'src/application/**/*.directive.js',
+  ])
+  .pipe(concat('application.js'))
+  .pipe(minify({
+    mangle: false
+  }))
+  .pipe(gulp.dest('cordova/www/js/'));
 
-    gulp
-        .src(['src/application/**/*.html'])
-        .pipe(gulp.dest('cordova/www/templates/'));
+  gulp
+  .src(['src/application/**/*.html'])
+  .pipe(gulp.dest('cordova/www/templates/'));
 
-    gulp
-        .src('src/index.html')
-        .pipe(gulp.dest('cordova/www/'));
+  gulp
+  .src('src/index.html')
+  .pipe(gulp.dest('cordova/www/'));
 });
 
 gulp.task('sass', function(done) {
-    gulp.src('src/scss/ionic.app.scss')
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(gulp.dest('cordova/www/css/'))
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '-min.css'
-        }))
-        .pipe(gulp.dest('cordova/www/css/'))
-        .on('end', done);
+  gulp.src('src/scss/ionic.app.scss')
+  .pipe(sass())
+  .on('error', sass.logError)
+  .pipe(gulp.dest('cordova/www/css/'))
+  .pipe(minifyCss({
+    keepSpecialComments: 0
+  }))
+  .pipe(rename({
+    extname: '-min.css'
+  }))
+  .pipe(gulp.dest('cordova/www/css/'))
+  .on('end', done);
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/application/**/*.js', ['build']);
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/application/**/*.js', ['build']);
 });
 
 gulp.task('install', function() {
-    bower.commands.install()
-        .on('log', function(data) {
-            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-        }).
-        on('end', function() {
-            gulp.start('default');
-        });
+  bower.commands.install()
+  .on('log', function(data) {
+    gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+  }).
+  on('end', function() {
+    gulp.start('default');
+  });
 });
+
--- src/application/application.module.js
+++ src/application/application.module.js
@@ -1,17 +1,17 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('application', ['ionic'])
-        .run(function($ionicPlatform) {
-            $ionicPlatform.ready(function() {
-                // Hide the accessory bar by default
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                }
-                if (window.StatusBar) {
-                    StatusBar.styleDefault();
-                }
-            });
-        })
+  angular
+  .module('application', ['ionic'])
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
 })();
