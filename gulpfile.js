b0ea9ba5d1939b4c66aaa229dbf45e55595983d5
Bruno Paixão <brunomrpx@gmail.com>
2015-12-09 23:33:42 -0200

    Adjustment on folder structure and nomenclature. Added static folder. Renamed cordova folder to build.

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,64 @@ var minify = require('gulp-minify');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
+var paths = {
+    src: 'src',
+    application: 'src/application',
+    scss: 'src/scss',
+    static: 'static',
+    build: 'build'
+};
+
 gulp.task('default', ['build', 'sass']);
 
 gulp.task('build', function() {
     gulp
-    .src([
-        'src/application/application.module.js',
-        'src/application/**/*.factory.js',
-        'src/application/**/*.service.js',
-        'src/application/**/*.controller.js',
-        'src/application/**/*.provider.js',
-        'src/application/**/*.directive.js',
-    ])
-    .pipe(concat('application.js'))
-    .pipe(minify({
-        mangle: false
-    }))
-    .pipe(gulp.dest('cordova/www/js/'));
+        .src([
+            paths.application + '/application.module.js',
+            paths.application + '/**/*.factory.js',
+            paths.application + '/**/*.service.js',
+            paths.application + '/**/*.controller.js',
+            paths.application + '/**/*.provider.js',
+            paths.application + '/**/*.directive.js',
+        ])
+        .pipe(concat('application.js'))
+        .pipe(minify({
+            mangle: false
+        }))
+        .pipe(gulp.dest(paths.build + '/www/js/'));
 
     gulp
-    .src(['src/application/**/*.html'])
-    .pipe(gulp.dest('cordova/www/templates/'));
+        .src([paths.application + '/**/*.html'])
+        .pipe(gulp.dest(paths.build + '/www/templates/'));
 
     gulp
-    .src('src/index.html')
-    .pipe(gulp.dest('cordova/www/'));
+        .src(paths.src + '/index.html')
+        .pipe(gulp.dest(paths.build + '/www/'));
+
+    gulp
+        .src(paths.static + '**/*', { base: paths.static })
+        .pipe(gulp.dest(paths.build + '/www/'));
 });
 
 gulp.task('sass', function(done) {
-    gulp.src('src/scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('cordova/www/css/'))
-    .pipe(minifyCss({
-        keepSpecialComments: 0
-    }))
-    .pipe(rename({
-        extname: '-min.css'
-    }))
-    .pipe(gulp.dest('cordova/www/css/'))
-    .on('end', done);
+    gulp
+        .src(paths.scss + '/ionic.app.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest(paths.build + '/www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '-min.css'
+        }))
+        .pipe(gulp.dest(paths.build + '/www/css/'))
+        .on('end', done);
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/application/**/*.js', ['build']);
+    gulp.watch(paths.scss + '/**/*.scss', ['sass']);
+    gulp.watch(paths.src + '/**/*', ['build']);
 });
 
 gulp.task('install', function() {
