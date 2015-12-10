5b6f7e09490fa32c1531435bbe65d44460b0a1d7
Bruno Paixão <brunomrpx@gmail.com>
2015-12-10 00:20:20 -0200

    Improved editor config to use indent_size = 2 in js files

--- gulpfile.js
+++ gulpfile.js
@@ -8,74 +8,74 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
 var paths = {
-    src: 'src',
-    application: 'src/application',
-    scss: 'src/scss',
-    static: 'static',
-    build: 'build'
+  src: 'src',
+  application: 'src/application',
+  scss: 'src/scss',
+  static: 'static',
+  build: 'build'
 };
 
 gulp.task('default', ['build', 'sass']);
 
 gulp.task('build', function() {
-    gulp
-        .src([
-            paths.application + '/application.module.js',
-            paths.application + '/**/*.factory.js',
-            paths.application + '/**/*.service.js',
-            paths.application + '/**/*.controller.js',
-            paths.application + '/**/*.provider.js',
-            paths.application + '/**/*.directive.js',
-        ])
-        .pipe(concat('application.js'))
-        .pipe(minify({
-            mangle: false
-        }))
-        .pipe(gulp.dest(paths.build + '/www/js/'));
+  gulp
+    .src([
+      paths.application + '/application.module.js',
+      paths.application + '/**/*.factory.js',
+      paths.application + '/**/*.service.js',
+      paths.application + '/**/*.controller.js',
+      paths.application + '/**/*.provider.js',
+      paths.application + '/**/*.directive.js',
+    ])
+    .pipe(concat('application.js'))
+    .pipe(minify({
+      mangle: false
+    }))
+    .pipe(gulp.dest(paths.build + '/www/js/'));
 
-    gulp
-        .src([paths.application + '/**/*.html'])
-        .pipe(gulp.dest(paths.build + '/www/templates/'));
+  gulp
+    .src([paths.application + '/**/*.html'])
+    .pipe(gulp.dest(paths.build + '/www/templates/'));
 
-    gulp
-        .src(paths.src + '/index.html')
-        .pipe(gulp.dest(paths.build + '/www/'));
+  gulp
+    .src(paths.src + '/index.html')
+    .pipe(gulp.dest(paths.build + '/www/'));
 
-    gulp
-        .src(paths.static + '**/*', { base: paths.static })
-        .pipe(gulp.dest(paths.build + '/www/'));
+  gulp
+    .src(paths.static + '**/*', { base: paths.static })
+    .pipe(gulp.dest(paths.build + '/www/'));
 });
 
 gulp.task('sass', function(done) {
-    gulp
-        .src(paths.scss + '/ionic.app.scss')
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(gulp.dest(paths.build + '/www/css/'))
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '-min.css'
-        }))
-        .pipe(gulp.dest(paths.build + '/www/css/'))
-        .on('end', done);
+  gulp
+    .src(paths.scss + '/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest(paths.build + '/www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({
+      extname: '-min.css'
+    }))
+    .pipe(gulp.dest(paths.build + '/www/css/'))
+    .on('end', done);
 });
 
 gulp.task('watch', function() {
-    gulp.watch(paths.scss + '/**/*.scss', ['sass']);
-    gulp.watch(paths.src + '/**/*', ['build']);
+  gulp.watch(paths.scss + '/**/*.scss', ['sass']);
+  gulp.watch(paths.src + '/**/*', ['build']);
 });
 
 gulp.task('install', function() {
-    bower
-        .commands
-        .install()
-        .on('log', function(data) {
-            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-        })
-        .on('end', function() {
-            gulp.start('default');
-        });
+  bower
+    .commands
+    .install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    })
+    .on('end', function() {
+      gulp.start('default');
+    });
 });
 
